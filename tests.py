0b7e4061555a0e28ad14e404db20d0fb01657476
Giorgio Grassini <giorgiograssini@gmail.com>
2019-01-31 11:29:38 +0100

    pulling

--- tests.py
+++ tests.py
@@ -1,20 +1,22 @@
 import unittest
-from viking import * 
+from vikingsClases import * 
 
-class TestStringMethods(unittest.TestCase):
+class MyTest(unittest.TestCase):
 
   strength = 150
   health = 300
-
   soldier = Soldier(health, strength)
 
-  def constructor_function(self):
-    self.assertEqual(Soldier.length, 2)
-    self.assertEqual(soldier.health, helath)
-    self.assertEqual(soldier.strenght, strenght)
-  
-  def attack_method(self):
-    self.assertEqual(typeof (soldier.attack), function)
+  def test(self):
+    strength = 150
+    health = 300
+    soldier = Soldier(health, strength)
+    # Soldier should recive 2 arguments
+    self.assertEqual(soldier.health, health)
+    self.assertEqual(soldier.strength, strength)
+    #self.assertEqual(type(soldier.attack), method)
+    #soldier attack should recive 0 arguments
+    self.soldier.attack()
 
 
 if __name__ == '__main__':

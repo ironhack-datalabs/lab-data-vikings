94e9f18747766a86de24198fd5fa32102f95740e
Giorgio Grassini <giorgiograssini@gmail.com>
2019-01-31 10:43:46 +0100

    first merge

--- tests.py
+++ tests.py
@@ -3,8 +3,18 @@ from viking import *
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual(hola(), 'hola')
+  strength = 150
+  health = 300
+
+  soldier = Soldier(health, strength)
+
+  def constructor_function(self):
+    self.assertEqual(Soldier.length, 2)
+    self.assertEqual(soldier.health, helath)
+    self.assertEqual(soldier.strenght, strenght)
+  
+  def attack_method(self):
+    self.assertEqual(typeof (soldier.attack), function)
 
 
 if __name__ == '__main__':

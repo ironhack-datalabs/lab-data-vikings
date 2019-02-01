8e52429a5322c7238fe599797af8d45a2942c126
potacho <potacho@gmail.com>
2019-02-01 18:23:23 +0100

    Creado soldier con errores

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,18 +1,20 @@
 #Project lab-data-vikings
 
 # Soldier (clase principal + constructor + 
-
-
 class Soldier:
-    def __init__(self, health, strength):
-	self.health = health
-	self.strength = strength
+  def __init__(self, health, strength):
+    self.health = health
+    self.strength = strength
+  
+  def attack():
+    return strength(self)
+
+  def receiveDamage(damage):
+    health()
 
 # Viking
-
-
 class Viking:
-    pass
+  pass
 
 # Saxon
 

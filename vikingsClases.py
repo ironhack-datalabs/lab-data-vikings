b688915d64e3a268602d836157e5754ee74dd09b
Pablo Ramos <pabloramosgomez@outlook.es>
2019-02-02 19:33:49 +0100

    Soldier y Vikings

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,13 +3,33 @@
 
 
 class Soldier:
-    pass
+    def __init__(self,health,strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self,damage):
+        self.health=self.health-damage
 
 # Viking
 
-
 class Viking:
-    pass
+    def __init__(name, self,health,strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self,damage):
+        self.health=self.health - damage
+        if self.health > 0:
+            return ("{} has received {} points of damage".format(self.name,damage))
+        else:
+            return ("{} has died in act of combat".format(self.name))
 
 # Saxon
 

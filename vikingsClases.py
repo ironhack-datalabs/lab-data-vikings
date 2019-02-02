a6d02c5dbff269ad624c0ff8fe5adde6610ad823
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-02-02 14:59:09 +0100

    3

--- vikingsClases.py
+++ vikingsClases.py
@@ -16,29 +16,29 @@ class Soldier:
 # Viking
 
 
-class Viking:
-    def __init__(self, name, health, strength):
+#class Viking:
+    #def __init__(self, name, health, strength):
         #inherit from soldier
-        self.name = name
-        self.health = health
-        self.strength = strength
+        #self.name = name
+        #self.health = health
+        #self.strength = strength
 
-    def attack(self):
-        return self.strength
+    #def attack(self):
+        #return self.strength
 
-    def receiveDamage(self, damage):
-        self.health = self.health - damage
+    #def receiveDamage(self, damage):
+        #self.health = self.health - damage
 
-    def battleCry(self, damage):
+    #def battleCry(self, damage):
 
 # Saxon
 
 
-class Saxon:
-    pass
+#class Saxon:
+    #pass
 
 # War
 
 
-class War:
-    pass
+#class War:
+    #pass

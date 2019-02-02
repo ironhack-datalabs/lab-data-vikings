941d12fdceec9b6e2a9a0e46fba923432e2bc5be
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-02-02 15:56:17 +0100

    5

--- vikingsClases.py
+++ vikingsClases.py
@@ -17,26 +17,44 @@ class Soldier:
 # Viking
 
 
-#class Viking:
-    #def __init__(self, name, health, strength):
-        #inherit from soldier
-        #self.name = name
-        #self.health = health
-        #self.strength = strength
+class Viking(Soldier):
+    pass
+    def __init__(self, name, health, strength):
+        Soldier.__init__(self, health, strength):
+        self.name = name
 
-    #def attack(self):
-        #return self.strength
+    def attack(self):
+        Soldier.def attack(self):
+        return self.strength
 
-    #def receiveDamage(self, damage):
-        #self.health = self.health - damage
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > :
+            return self.name + "has received" + self.damage + points of damage
+        else:
+            return self.name + "has died in combat"
 
-    #def battleCry(self, damage):
+    def battleCry(self, damage):
+        return "Odin Owns You All!"
 
 # Saxon
 
 
-#class Saxon:
-    #pass
+class Saxon(Soldier):
+    pass
+    def __init__(self, health, strength):
+        Soldier.__init__(self, health, strength):
+    
+    def attack(self):
+        Soldier.def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > :
+            return self.name + "has received" + self.damage + points of damage
+        else:
+            return self.name + "has died in combat"
 
 # War
 

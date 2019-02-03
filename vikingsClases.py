4abb42b78738e8c846983b611b747a81273ab198
Rodrigo Hernandez <rodrihercif@gmail.com>
2019-02-03 14:27:44 +0100

    first commit

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,13 +3,40 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__
+        self.name = name
+
+    def attack(self):
+        super().attack(self)
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if health > 0:
+            alive_msg = (
+                "{} has received {} points of damage".format(self.name, damage))
+            return alive_msg
+        elif health == 0:
+            dead_msg = ("{} has died in act of combat".format(self.name))
+            return dead_msg
+
+    def battleCry():
+        return ("Odin Owns You All!")
+
 
 # Saxon
 

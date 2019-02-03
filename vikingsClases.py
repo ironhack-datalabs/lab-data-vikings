526873aa1b5bb5fb5d7c66411573ba06a2613728
Livia Canet <liviacanets@gmail.com>
2019-02-03 13:33:08 +0100

    Commit

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,24 +1,77 @@
+import numpy as np
+
+
 
 # Soldier
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    
+    #Methods
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = health - damage
+    
+    
+    
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        
+    
+    #Methods (attack method is inherited from Soldier class)
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = health - damage
+        if self.health > 0:
+            return print("{} has received {} points of damage.".format(self.name, self.damage))
+        else:
+            return print("{} has died in act of combat.".format(self.name))
+        
+    def battleCry(self):
+        return print("Odin Owns You All!")
+    
+    
+
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+        
+    
+    #Methods (attack method is inherited from Soldier class)
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = health - damage
+        if self.health > 0:
+            return print("A Saxon has received {} points of damage.".format(self.damage))
+        else:
+            return print("A Saxon has died in combat.")
+        
+        
+
 
 # War
 
 
 class War:
-    pass
+    def __init__(self, vikingArmy, saxonArmy):
+        self.vikingArmy = np.array(vikingArmy)
+        

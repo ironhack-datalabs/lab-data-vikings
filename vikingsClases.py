7ff36f489c1a9bc949e619e2a479f61f5192f340
potacho <potacho@gmail.com>
2019-02-03 23:46:15 +0100

    Soldier

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,16 +1,18 @@
 #Project lab-data-vikings
 
-# Soldier (clase principal + constructor + 
-class Soldier:
-  def __init__(self, health, strength):
-    self.health = health
-    self.strength = strength
-  
-  def attack():
-    return strength(self)
 
-  def receiveDamage(damage):
-    health()
+# Soldier (constructor, ataque y daño)
+class Soldier:
+  #Constructor: contiene las propiedades self, health y strength
+  def __init__(self, health, strength):
+    self.health = healt
+    self.strength = strength
+  #Método 'attack' no recive ninguna variable aparte de self. Este método devuelve la propiedad 'strength'.
+  def attack(self):
+    return self.strength
+  #Método 'receiveDamage' recibe la variable daño ('skadi') la cual afecta a la propiedad 'health'
+  def receiveDamage(self, skadi):
+    self.health = self.health - skadi
 
 # Viking
 class Viking:

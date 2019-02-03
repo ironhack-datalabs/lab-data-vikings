ce4f6a115270536b48b790ac772db3bc639adc7d
Paula <postigorpaula@gmail.com>
2019-02-03 22:18:46 +0100

    Terminado soldier, viking y saxos test ok

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,24 +1,84 @@
 
+import random
+
+
 # Soldier
 
-
 class Soldier:
-    pass
+
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = self.health - self.damage
+
 
 # Viking
 
+class Viking (Soldier):
 
-class Viking:
+    def __init__(self, name, health, strength):
+
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+
+        if self.health > 0:
+            return "{} has received {} points of damage".format(self.name, self.damage)
+        else:
+            return "{} has died in act of combat".format(self.name)
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+
     pass
+
 
 # Saxon
 
+class Saxon (Soldier):
 
-class Saxon:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+
+        if self.health > 0:
+            return "A Saxon has received {} points of damage".format(self.damage)
+        else:
+            return "A Saxon has died in combat"
+
     pass
+
 
 # War
 
-
 class War:
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        random.choice(self.vikingArmy)
+
+    def saxonAttack(self):
+
+    showStatus()
+
     pass

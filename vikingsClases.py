ccce48e78eb7ca40faa8d82deea6a8f7260231f2
Livia Canet <liviacanets@gmail.com>
2019-02-03 15:47:30 +0100

    Commit

--- vikingsClases.py
+++ vikingsClases.py
@@ -42,7 +42,8 @@ class Viking(Soldier):
             return print("{} has died in act of combat.".format(self.name))
         
     def battleCry(self):
-        return print("Odin Owns You All!")
+        self.viking.battleCry = print("Odin Owns You All!")
+        return self.viking.battleCry
     
     
 

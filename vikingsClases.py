51203955e80cb6a5d4ea02aca20f4a2f370176ed
iamjss <jbrantuas@gmail.com>
2019-04-17 16:16:16 +0200

    Hecha clase de Soldier y pasados tests

--- vikingsClases.py
+++ vikingsClases.py
@@ -6,7 +6,13 @@ class Soldier:
     def __init__(self, health, strength):
         self.health = health
         self.strength = strength
-    
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+
 
 # Viking
 

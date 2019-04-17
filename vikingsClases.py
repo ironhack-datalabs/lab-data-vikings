95b358d983c9a8ded97aa714048c9a757155635a
vicenteccastillo <castillocasarejos.vicentec@gmail.com>
2019-04-17 19:04:44 +0200

    Ejercicio hasta la guerra

--- vikingsClases.py
+++ vikingsClases.py
@@ -7,23 +7,41 @@ class Soldier:
      self.health = health
      self.strength = strength
 
-
+    def attack(self): 
+        return self.strength
+        
+    def receiveDamage(self,damage):
+        self.health -= damage
 
 
 # Viking
 
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        super().__init__(health,strength)
+        self.strength = strength
 
-class Viking:
-    pass
+    def receiveDamage(self,damage):
+        super().receiveDamage(damage)
+        if self.health >0:
+            return ("{} has received {} points of damage" .format(self.name, damage))
+        else:
+            return ("{} has died in act of combat".format(self.name))
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
+class Saxon(Soldier):
+    def receiveDamage(self,damage):
+        super().receiveDamage(damage)
+        if self.health >0:
+            return ("A Saxon has received {} points of damage" .format(damage))
+        else:
+            return ("A Saxon has died in combat")
+# Wars
 
-class Saxon:
-    pass
-
-# War
-
-
-class War:
-    pass
+class War():
+    def__init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []

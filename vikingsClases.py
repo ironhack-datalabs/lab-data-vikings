e2e3d9b1d8801a05838dfebd426c299fd22118a8
Martin Molestina L <martin.molestina@gmail.com>
2019-04-17 16:05:49 +0200

    soldier hasta recieve damage

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,9 +1,19 @@
 
-# Soldiers
+# Soldier
 
 
-class Soldier:
-    pass
+class Soldier():
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+
+
 
 # Viking
 

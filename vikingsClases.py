c426f1668877c160a4b67fa59afa7f74720ff3c1
Martin Molestina L <martin.molestina@gmail.com>
2019-04-17 20:49:34 +0200

    vikings terminado

--- vikingsClases.py
+++ vikingsClases.py
@@ -18,8 +18,20 @@ class Soldier():
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        super().__init__(health, strength)
+    
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > 0:
+            return '{} has received {} points of damage'.format(self.name, damage)
+        else:
+            return '{} has died in act of combat'.format(self.name)
+
+    def battleCry(self):
+        return 'Odin Owns You All!'
 
 # Saxon
 

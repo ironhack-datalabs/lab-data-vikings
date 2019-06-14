719c64be013c8df17b20469c00e5e384cf24fc6e
fserrey <fserrey@gmail.com>
2019-06-14 18:18:00 +0200

    Soldiers tests passed

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,7 +3,17 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.strength = strength
+        self.health = health
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= damage
+
 
 # Viking
 

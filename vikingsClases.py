58ee0a419a2e7208b5f76f3cdb536850d4443d70
CLARA FERNANDEZ VALLES <cm.fernandezv13@gmail.com>
2019-06-14 20:21:26 +0200

    test 2 pasado

--- vikingsClases.py
+++ vikingsClases.py
@@ -13,14 +13,22 @@ class Soldier:
         self.health -= damage
 
 
-# Viking
 
-"""
-class Viking:
-    def __init__(self, health, strength, name):
-        super(Viking, self).__init__(health, strength)
-        self.name=name
-"""
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super(Viking, self).__init__(health,strength)
+        self.name = name
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.damage=damage
+        self.health -= damage
+        if self.health> 0: 
+            return  "{} has received {} points of damage".format(self.name, self.damage)
+        elif self.health <= 0:
+            return  "{} has died in act of combat".format(self.name)
+    def battleCry (self):
+        return "Odin Owns You All!"  
 # Saxon
 
 

d3594e8c2982596659036011f92007266fbcf270
Javier Díaz <jdcaracuel@gmail.com>
2019-06-14 21:24:48 +0200

    Por las barbas de Odín

--- vikingsClases.py
+++ vikingsClases.py
@@ -17,16 +17,47 @@ class Soldier:
 # Viking
 
 
-class Viking:
+class Viking (Soldier):
+    def __init__(self, name, health, strength):
+        super(Viking, self).__init__(health, strength)
+        self.name = name
+
+
+class Viking (Soldier):
+
+    def __init__(self, name, health, strength):
+
+        super(Viking, self).__init__(health, strength)
+
+        self.name = name
+
+    def receiveDamage(self, damage):
+
+        self.damage = damage
+
+        self.health -= damage
+
+        if self.health > 0:
+
+            return "{} has received {} points of damage".format(self.name, damage)
+
+        else:
+
+            return "{} has died in act of combat".format(self.name)
+
+    def battleCry(self):
+
+        return "Viva la virgen de la Macarena"
+
+
+class Saxon(Soldier):
+    def __init__(self, receiveDamage, health, strength):
+        super(Saxon, self).__init__(health, strength)
+        self.receiveDamage
+
+
+
     pass
-
-# Saxon
-
-
-class Saxon:
-    pass
-
-# War
 
 
 class War:

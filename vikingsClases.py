de78d55f3dbb5a85345970eca8f78612eceddb1b
fserrey <fserrey@gmail.com>
2019-06-14 19:47:24 +0200

    Odin owns you all! - Vikings objects done

--- vikingsClases.py
+++ vikingsClases.py
@@ -18,17 +18,34 @@ class Soldier:
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super(Viking, self).__init__(health, strength)
+        self.name = name
 
-# Saxon
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= damage
+        if self.health > 0:
+            return "{} has received {} points of damage".format(self.name, self.damage)
+        if self.health <= 0:
+            return "{} has died in act of combat".format(self.name)
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+        pass
 
 
-class Saxon:
-    pass
+"""class Saxon:
+
+
+pass
 
 # War
 
 
 class War:
-    pass
+
+
+pass
+"""

e81a9778a0e2482be6d166c8db58aaa20fcdb023
aiborra11 <iborra.11@gmail.com>
2019-06-14 18:14:36 +0200

    soldier

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,9 +3,34 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
 
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health = health - damage
+
+
+"""
+
+
+#### `attack()` method
+
+- should be a function
+- should receive **0 arguments**
+- should return **the `strength` property of the `Soldier`**
+
+#### `receiveDamage()` method
+
+- should be a function
+- should receive **1 argument** (the damage)
+- should remove the received damage from the `health` property
+- **shouldn't return** anything
 # Viking
+"""
 
 
 class Viking:

7c5443d8cded50b9c5933b42e6840b71f21a1a9b
Gori89 <gori897@gmail.com>
2019-08-26 19:05:09 +0200

    Count Attacks

--- battle.py
+++ battle.py
@@ -10,13 +10,20 @@ def createViking(num):
 def createSajon():
     return vc.Saxon(random.randint(200,250),random.randint(15,20))
 
+vikingAttack=0
+saxonAttack=0
+
 #Attack
 def attack(war):
     if random.choice( ['viking', 'saxon'] )=='viking':
+        global vikingAttack
+        global saxonAttack
         print("=========Ataque vikingo=======")
+        vikingAttack+=1
         return war.vikingAttack()
     else: 
         print("=========Ataque sajon=======")
+        saxonAttack+=1
         return war.saxonAttack()
 
 
@@ -28,13 +35,16 @@ for i in range(random.randint(10,15)):
     war.addViking(createViking(i))
 
 # Saxon Army
-for i in range(random.randint(20,25)):
+for i in range(random.randint(30,35)):
     war.addSaxon(createSajon())
+
 
 while len(war.saxonArmy)!=0 and len(war.vikingArmy)!=0:
     print(attack(war))
     print(war.showStatus())
     print(str(len(war.vikingArmy))+" Vikings - "+str(len(war.saxonArmy))+" Saxons")
+    print("Saxon attacks: "+str(saxonAttack))
+    print("Viking attacks: "+str(vikingAttack))
 
 
 

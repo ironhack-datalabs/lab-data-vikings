6fdde935526f14881011f0b8a59bef6ca67825cc
rsd13 <rsoriadiez@gmail.com>
2019-08-26 16:01:24 +0200

    Clase vikingos terminada y pasan todos los test

--- vikingsClases.py
+++ vikingsClases.py
@@ -17,12 +17,28 @@ class Soldier:
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        self.name = name
+        #llamamos a super 
+        Soldier.__init__(self,health,strength)
+
+
+
+    def receiveDamage(self,damage):
+        
+        self.health -= damage
+        if self.health > 0:
+            return self.name + " has received " + str(damage) + " points of damage"
+        else:
+            return self.name + " has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+
 
 # Saxon
-
-
+    
 class Saxon:
     pass
 

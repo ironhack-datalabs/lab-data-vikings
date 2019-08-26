175640b855bec383c609bd795000ab81d7617347
rsd13 <rsoriadiez@gmail.com>
2019-08-26 16:59:43 +0200

    Ejercicios terminados

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,3 +1,5 @@
+import random
+
 
 # Soldier
 
@@ -51,4 +53,57 @@ class Saxon(Soldier):
 
 
 class War:
-    pass
+
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+
+    def addViking(self,Viking):
+        self.vikingArmy.append(Viking)
+
+
+    def addSaxon(self,Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        #se elige un numero aleatorio desde el principio hasta el final de la lista
+        sizeSaxon = len(self.saxonArmy)
+        sizeViking = len(self.vikingArmy)
+        saxonRandom = self.saxonArmy[random.randrange(0,sizeSaxon)]
+        vikingRandom = self.vikingArmy[random.randrange(0,sizeViking)]
+        isLive =saxonRandom.receiveDamage(vikingRandom.attack())
+
+        if sizeSaxon and isLive == "A Saxon has died in combat":
+            self.saxonArmy.remove(saxonRandom)
+
+        return isLive
+
+    def saxonAttack(self):
+        sizeSaxon = len(self.saxonArmy)
+        sizeViking = len(self.vikingArmy)
+        saxonRandom = self.saxonArmy[random.randrange(0,sizeSaxon)]
+        vikingRandom = self.vikingArmy[random.randrange(0,sizeViking)]
+        isLive =vikingRandom.receiveDamage(saxonRandom.attack())
+        
+        if sizeViking and vikingRandom.name + " has died in act of combat" == isLive:
+            self.vikingArmy.remove(vikingRandom)
+        
+
+        return isLive
+
+    def showStatus(self):
+        if not self.saxonArmy:
+            return "Vikings have won the war of the century!"
+        if not self.vikingArmy:
+            return "Saxons have fought for their lives and survive another day..."
+        if self.vikingArmy and self.saxonArmy:
+            return "Vikings and Saxons are still in the thick of battle."
+
+
+
+
+
+
+
+

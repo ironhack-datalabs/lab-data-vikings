99d7a4fc93db21d10d9dece5ae6ea69aa920252e
Dededavid <davidperez2121@gmail.com>
2019-08-26 19:48:49 +0200

    trabajo hasta empezar la guerra

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,24 +1,57 @@
 
 # Soldier
 
-
-class Soldier:
-    pass
+class Soldier():
+    def __init__(self, health, strength): 
+        self.health = health
+        self.strength = strength
+        
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage): 
+        self.health = self.health - damage 
+    
+    
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__ (self, name, health, strength):
+        Soldier.__init__(self, health, strength)
+        self.name = name
+        
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+            self.health -= damage
+            if self.health > 0: 
+                return(str(self.name) + " has received " + str(damage) + " points of damage")
+            else:
+                return(str(self.name) + " has died in act of combat")
+                       
+    def battleCry(self):
+        return ("Odin Owns You All!")
+                
+    
 
 # Saxon
 
+class Saxon(Soldier):
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "A Saxon has received " + str(damage) + " points of damage"
+        else:
+            return "A Saxon has died in combat"
+    
+    
+    
 
-class Saxon:
-    pass
 
 # War
-
 
 class War:
     pass

24d662ca7e46183e39cd81bd312e19d98478fa07
Ines P. Chavarri <inespchavarri@gmail.com>
2019-08-26 17:24:34 +0200

    soldier resuelto

--- vikingsClases.py
+++ vikingsClases.py
@@ -4,22 +4,71 @@
 
 class Soldier:
     def __init__(self, health, strength):
-    pass
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        health - damage
+        if health > damage:
+            return self.name + " has received" + self.damage + " points of damage"
+        elif healt <= damage:
+            return self.name + " has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+        pass
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack():
+        pass
+    def recieveDamage():
+        pass
 
 # War
 
 
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+
+# War métodos
+
+    def addViking():
+        pass
+    def addSaxon():
+        pass
+    def vikingAttack():
+        pass
+    def saxonAttack():
+        pass
+    def showStatus():
+        pass
+   

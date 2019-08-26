f4e2004bc321ab2c45324fa286c4864ecbc5f1cd
Nacho-laiseca <ilaiseca@hotmail.com>
2019-08-26 17:14:16 +0200

    lab-data-vikings: Viking  pass

--- vikingsClases.py
+++ vikingsClases.py
@@ -28,19 +28,28 @@ class Viking(Soldier):
         self.name = name
         self.health = health
         self.strength = strength
-    def receiveDamage(self,damage=0):
+
+    def receiveDamage(self, damage=0):
         if (self.health - damage) <= 0:
+            self.health = 0
             return self.name + " has died in act of combat"
         else:
+            self.health -= damage
             return self.name + " has received " + str(damage) + " points of damage"
 
-
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self,health,strength)
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self,damage=0):
+        
 
 # War
 

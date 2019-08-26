7755b53612591fafe369f3625dd3f406c2ebdffe
Curro <farc@outlook.com>
2019-08-26 16:08:03 +0200

    class Viking ok

--- vikingsClases.py
+++ vikingsClases.py
@@ -17,8 +17,20 @@ class Soldier:
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self,name,health,strenght):
+        Soldier.__init__(self, health, strenght)
+        self.name = name
+    
+    def receiveDamage(self,damage):
+        self.health = self.health - damage
+        if self.health>0:
+            return "{} has received {} points of damage".format(self.name,damage)
+        else:
+            return "{} has died in act of combat".format(self.name)
+    
+    def battleCry(self):
+        return "Odin Owns you All!"
 
 # Saxon
 

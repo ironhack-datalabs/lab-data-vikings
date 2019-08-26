21cf1348e8211981a207d3dcd407a53f920c7f59
Gonzalo Bandeira <gonzalobandeira@gmail.com>
2019-08-26 15:38:50 +0200

    start lab

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,15 +1,33 @@
 
 # Soldier
-
-
 class Soldier:
-    pass
+    def __init__(self,health,strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self): 
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        Soldier.__init__(self,health,strength)
+        self.name = name
+    
+    def receiveDamage(self):
+        pass
+    
+    def battleCry(self):
+        pass
+
+    
+
+    
 
 # Saxon
 
@@ -22,3 +40,7 @@ class Saxon:
 
 class War:
     pass
+
+
+v = Viking(1,2,"GOnzalo")
+print(v.health)

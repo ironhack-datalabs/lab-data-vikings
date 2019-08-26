8e3d7227ec6dc20c2330dad6431a2013c2643a3c
Gori89 <gori897@gmail.com>
2019-08-26 15:59:08 +0200

    Soldier

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,7 +3,16 @@
 
 
 class Soldier:
-    pass
+
+    def __init__(self, health=0 , strength=0):
+        self.health=health
+        self.strength=strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        self.health-=damage
 
 # Viking
 

c1be6196ddb182ead984287cb3f536b5d6a826cc
Antonio Pons <toninopons@gmail.com>
2019-10-28 19:40:45 +0100

    Todavia haciendo el bonus

--- vikingsClases.py
+++ vikingsClases.py
@@ -42,13 +42,35 @@ class Saxon(Soldier):
 
 # War
 
-
+import random
 class War:
     def __init__(self):
         self.vikingArmy = []
         self.saxonArmy = []
     def addViking(self, Viking):
-        self.vikingArmy = self.vikingArmy.append(Viking)
+        self.vikingArmy.append(Viking)
     def addSaxon(self, Saxon):
-        self.saxonArmy = self.saxonArmy.append(Saxon)
+        self.saxonArmy.append(Saxon)
     def vikingAttack(self):
+        random_saxon = random.choice(self.saxonArmy)
+        random_viking = random.choice(self.vikingArmy)
+        random_saxon.receiveDamage(random_viking.strength)
+        if random_saxon.health <= 0:
+            del random_saxon
+        else:
+            return random_saxon
+    def saxonAttack(self):
+        random_saxon = random.choice(self.saxonArmy)
+        random_viking = random.choice(self.vikingArmy)
+        random_viking.receiveDamage(random_saxon.strength)
+        if random_viking.health <= 0:
+            del random_viking
+        else:
+            return random_viking
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return f"Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return f"Saxons have fought for their lives and survive another day..."
+        elif (len(self.saxonArmy) != 0) & (len(self.vikingArmy) != 0):
+            return f"Vikings and Saxons are still in the thick of battle."

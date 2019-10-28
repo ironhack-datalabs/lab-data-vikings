229c1a706a8f98e382468529336d53a633ffaede
Ovi <jm.fdez.corbacho@gmail.com>
2019-10-28 16:49:49 +0100

    Añadido hasta Saxon

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,6 +1,5 @@
 
 # Soldier
-
 class Soldier:
     def __init__ (self, health, strength):
         self.health = health
@@ -14,16 +13,12 @@ class Soldier:
 
 
 # Viking
-
 class Viking (Soldier):
     def __init__ (self, name, health, strength):
         self.name = name
         self.health = health
         self.strength = strength
 
-    def attack (self):
-        return super().attack()
-    
     def receiveDamage (self, damage):
         self.health -= damage
         if self.health > 0:
@@ -34,11 +29,16 @@ class Viking (Soldier):
     def battleCry (self):
         return ("Odin Owns You All!")
 
+
 # Saxon
+class Saxon (Soldier):
+    def receiveDamage (self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return ("A Saxon has received {} points of damage".format(damage))
+        else:
+            return ("A Saxon has died in combat")
 
-
-class Saxon:
-    pass
 
 # War
 

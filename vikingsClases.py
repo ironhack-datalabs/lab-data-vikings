760ca2aec14f3fac68ae170a7793baf966e9bc88
AlexMndzF <alex.93menendez@gmail.com>
2019-10-28 18:36:04 +0100

    War class terminado

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,4 +1,5 @@
 
+import random
 # Soldier
 
 
@@ -18,11 +19,11 @@ class Soldier:
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        Soldier.__init__(self, health, strength)
+        super().__init__(health, strength)
         self.name = name
 
     def receiveDamage(self, damage):
-        self.health -= damage
+        super().receiveDamage(damage)
         if self.health > 0:
             return ('{} has received {} points of damage'.format(self.name, damage))
         else:
@@ -36,10 +37,10 @@ class Viking(Soldier):
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        Soldier.__init__(self, health, strength)
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        self.health -= damage
+        super().receiveDamage(damage)
         if self.health > 0:
             return ('A Saxon has received {} points of damage'.format(damage))
         else:
@@ -47,5 +48,39 @@ class Saxon(Soldier):
 # War
 
 
-class War:
-    pass
+class War():
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+
+    def addSaxon(self, Saxson):
+        self.saxonArmy.append(Saxson)
+
+    def vikingAttack(self):
+        saxsonpelea = random.choice(self.saxonArmy)
+        vikingpelea = random.choice(self.vikingArmy)
+        result = saxsonpelea.receiveDamage(vikingpelea.attack())
+        if saxsonpelea.health <= 0:
+            self.saxonArmy .remove(saxsonpelea)
+        return result
+
+    def saxonAttack(self):
+        saxsonpelea = random.choice(self.saxonArmy)
+        vikingpelea = random.choice(self.vikingArmy)
+        result = vikingpelea.receiveDamage(saxsonpelea.attack())
+        if vikingpelea.health <= 0:
+            self.vikingArmy.remove(vikingpelea)
+        return result
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy) != 0 and len(self.vikingArmy) != 0:
+            return "Vikings and Saxons are still in the thick of battle."
+
+        pass

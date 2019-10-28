359ed1527ebcbc6e1d60607a7c455048d90c4fb8
alfpml <alfpml@gmail.com>
2019-10-28 17:31:57 +0100

    commit saxons

--- vikingsClases.py
+++ vikingsClases.py
@@ -11,43 +11,39 @@ class Soldier:
     
     def receiveDamage(self,damage):
         self.health=self.health-damage 
-     
-'''
-attack() method
+        
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        self.name=name
+        self.health=health
+        self.strength=strength
 
-    should be a function
-    should receive 0 arguments
-    should return the strength property of the Soldier
+    def receiveDamage(self,damage):
+        self.health=self.health-damage
+        if self.health>0:
+            return "{} has received {} points of damage".format(self.name,damage)
+        else:
+            return "{} has died in act of combat".format(self.name)
 
-receiveDamage() method
+    def battleCry(self):
+        return "Odin Owns You All!"
 
-    should be a function
-    should receive 1 argument (the damage)
-    should remove the received damage from the health property
-    shouldn't return anything
-
- '''
-# Viking
-
-
-class Viking:
-    def __init__(self,name,strength,health):
-        self.name = 'Harald'
-        self.strength = strength
-        self.health = health
-    pass
 
 '''
+Saxon
+
+A Saxon is a weaker kind of Soldier. Unlike a Viking, a Saxon has no name. Their receiveDamage() method will also be different than the original Soldier version.
+
+Modify the Saxon, constructor function, have it inherit from Soldier and reimplement the receiveDamage() method for Saxon.
 inheritance
 
-    Viking should inherit from Soldier
+    Saxon should inherit from Soldier
 
 constructor function
 
-    should receive 3 arguments (name, health & strength)
-    should receive the name property as its 1st argument
-    should receive the health property as its 2nd argument
-    should receive the strength property as its 3rd argument
+    should receive 2 arguments (health & strength)
+    should receive the health property as its 1st argument
+    should receive the strength property as its 2nd argument
 
 attack() method
 
@@ -55,25 +51,19 @@ attack() method
 
     should be a function
     should receive 0 arguments
-    should return the strength property of the Viking
+    should return the strength property of the Saxon
 
 receiveDamage() method
 
-(This method needs to be reimplemented for Viking because the Viking version needs to have different return values.)
+(This method needs to be reimplemented for Saxon because the Saxon version needs to have different return values.)
 
     should be a function
     should receive 1 argument (the damage)
     should remove the received damage from the health property
-    if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
-    if the Viking dies, it should return "NAME has died in act of combat"
+    if the Saxon is still alive, it should return "A Saxon has received DAMAGE points of damage"
+    if the Saxon dies, it should return "A Saxon has died in combat"
 
-battleCry() method
 
-Learn more about battle cries.
-
-    should be a function
-    should receive 0 arguments
-    should return "Odin Owns You All!"
 
 '''
 
@@ -81,11 +71,17 @@ Learn more about battle cries.
 # Saxon
 
 
-class Saxon:
-    def __init__(self,strength,health):
-        self.strength = strength
-        self.health = health
+class Saxon(Soldier):
+    def __init__(self,health,strength):
+        self.health=health
+        self.strength=strength
 
+    def receiveDamage(self,damage):
+        self.health=self.health-damage
+        if self.health>0:
+            return "A Saxon has received {} points of damage".format(damage)
+        else:
+            return "A Saxon has died in combat"
     pass
 
 # War

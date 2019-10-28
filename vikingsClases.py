40acf9cf90b31b42dcb743a6613b1ec1e14e3b48
Antonio Pons <toninopons@gmail.com>
2019-10-28 17:27:14 +0100

    Soldier listo, Viking listo

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,13 +3,29 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 

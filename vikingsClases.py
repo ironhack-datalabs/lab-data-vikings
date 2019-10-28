4a2840de297187e626ada2d3c1a8d3f3368c29e2
Joaquin <jperezdeayala@gmail.com>
2019-10-28 17:14:25 +0100

    Saxons

--- vikingsClases.py
+++ vikingsClases.py
@@ -37,8 +37,17 @@ class Viking(Soldier):
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "A Saxon has received {} points of damage".format(damage)
+        else:
+            return "A Saxon has died in combat"
 
 # War
 

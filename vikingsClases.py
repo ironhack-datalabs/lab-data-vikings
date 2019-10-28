51cf575a86a84a5e2956b9d1851d90aea713081e
AlexMndzF <alex.93menendez@gmail.com>
2019-10-28 15:35:35 +0100

    Soilders terminado

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,7 +3,15 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 

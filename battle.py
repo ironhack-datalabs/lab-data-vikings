5c53fb3083bca9df7c9ca854504e57a383212ad2
Álvaro Rivas <alvarorivasg26@gmail.com>
2019-10-29 09:26:29 +0100

    mejora

--- battle.py
+++ battle.py
@@ -9,16 +9,15 @@ batalla.addViking(Viking('El Rober', 78, 160))
 batalla.addSaxon(Saxon(89, 178))
 batalla.addSaxon(Saxon(98, 189))
 batalla.addSaxon(Saxon(88, 176))
+batalla.addViking(Viking('Tu_nene', 123, 130))
 ataque1 = batalla.vikingAttack
 ataque2 = batalla.saxonAttack
-lista=[ataque1,ataque2]
+lista = [ataque1, ataque2]
 
 #imposible más aleatoriedad...
-while True:
+while len(batalla.vikingArmy) != 0 and len(batalla.saxonArmy) != 0:
     random.choice(lista)()
     print(batalla.showStatus())
-    if len(batalla.vikingArmy) == 0 or len(batalla.saxonArmy) == 0:
-        break
 
 #los vikingos siempre empiezan
 '''while True:

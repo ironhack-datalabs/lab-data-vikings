b50849419c5c81a82c8cbcb64927b679caf01b97
Santiago Bermejo <sbermejosuanzes@gmail.com>
2020-01-17 18:16:24 +0100

    resolving war

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,21 +1,50 @@
 
 # Soldier
 
-
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+  
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
 
 # Viking
 
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+        
+    def attack(self):
+        return super().attack()
+        
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return self.name + " has received " + str(damage) + " points of damage"
+        else:
+            return self.name + " has died in act of combat"
+            
+    def battleCry(self):
+        return "Odin Owns You All!"
 
-class Viking:
-    pass
-
-# Saxon
-
-
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+        
+    def attack(self):
+        return super().attack()
+        
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "A Saxon has received" + str(damage) + "points of damage"
+        else:
+            return "A Saxon has died in combat"
 
 # War
 

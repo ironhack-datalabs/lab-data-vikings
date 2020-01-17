3d9ff71d8b6f10f87a686bd265be09fbd6101b76
lauravillalba <lvillalba@live.com>
2020-01-17 16:37:55 +0100

    testSoldier-OK

--- vikingsClases.py
+++ vikingsClases.py
@@ -3,15 +3,23 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+
 
 # Viking
 
 
 class Viking:
-    pass
 
-# Saxon
+    # Saxon
 
 
 class Saxon:

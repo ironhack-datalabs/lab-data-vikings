ace4d6d540cb3977adc0646e8844c58aae71dc66
Mariló Molina Candela <hec.lopezvizcaino@gmail.com>
2020-01-18 14:46:17 +0100

    Todo menos los fcking vikingAttack & saxonAttack de class War

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,3 +1,4 @@
+import random
 
 # Soldier
 
@@ -41,10 +42,46 @@ class Saxon(Soldier):
     def __init__(self, health, strength):
         super().__init__(health, strength)
 
-    def receiveDamage(self):
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "A Saxon has received " +  str(damage) + " points of damage"
+        else:
+            return "A Saxon has died in combat"
 
 
 # War
 
 
-#class War:
+class War():
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+    
+    def addSaxon(self,Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        self.randomViking = random.choice(vikingArmy)
+        self.randomSaxon = random.choice(saxonArmy)
+        super(Saxon).receiveDamage(strength)
+        if health(Saxon) == 0:
+            self.saxonArmy.remove(Saxon)
+        pass
+    
+    def saxonAttack(self):
+        pass
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+    
+
+        

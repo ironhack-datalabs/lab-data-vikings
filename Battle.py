24ad1caced3d1d18673ac3dee7d36f505eedc72c
Irene Lopez <irene.lopezs@protonmail.com>
2020-01-18 19:03:25 +0100

    Clase soldier, viking y saxon creada.

---
+++ Battle.py
@@ -0,0 +1,10 @@
+import vikings as vk #por ej. para importar las clases que tengo en TestSoldier
+
+- Generar una batalla
+
+- Variable (war) con los atributos
+
+- Funcion que cree sajones, funcion que cree vikingos
+
+- Una vez tengo la lista de los ejercitos empiezo a pelear hasta que no queden mas vikingos o sajones (con un while)
+
--- vikingsClases.py
+++ vikingsClases.py
@@ -2,20 +2,49 @@
 # Soldier
 
 
-class Soldier:
-    pass
+class Soldier():
+    def __init__(self, health, strength):
+       self.health = health #valor de la variable
+       self.strength = strength
 
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+    
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "{} has received {} points of damage".format(self.name, )
+        return "{} has died in act of combat".format(self.name)
+
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    #def __init__(self, health, strength):
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "A Saxon has received {} points of damage".format(damage)
+        return "A Saxon has died in combat"
+
 
 # War
 

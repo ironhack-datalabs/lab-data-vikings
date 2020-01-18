e432cb68798928d0c896b1e9ffc36f35a5d38012
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-01-18 15:42:25 +0100

    Terminado lab vikings

--- vikingsClases.py
+++ vikingsClases.py
@@ -1,3 +1,4 @@
+import random
 
 # Soldier
 
@@ -45,6 +46,37 @@ class Saxon(Soldier):
 
 # War
 
-
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking_1):
+        self.vikingArmy.append(viking_1)
+
+    def addSaxon(self, saxon_1):
+        self.saxonArmy.append(saxon_1)
+
+    def vikingAttack(self):
+        sax = random.choice(self.saxonArmy)
+        vik = random.choice(self.vikingArmy)
+        vikatt = sax.receiveDamage(vik.strength)
+        if sax.health <= 0:
+            self.saxonArmy.remove(sax)
+        return vikatt
+
+    def saxonAttack(self):
+        sax = random.choice(self.saxonArmy)
+        vik = random.choice(self.vikingArmy)
+        saxatt = vik.receiveDamage(sax.strength)
+        if vik.health <= 0:
+            self.vikingArmy.remove(vik)
+        return saxatt
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        elif len(self.saxonArmy) >= 1 and len(self.vikingArmy) >= 1:
+            return "Vikings and Saxons are still in the thick of battle."

5d95f31fd8d588daa721739a5ae1d07748882d71
lauravillalba <lvillalba@live.com>
2020-01-19 14:56:13 +0100

    Bonus Finalizado

--- vikingsClases.py
+++ vikingsClases.py
@@ -27,7 +27,6 @@ class Viking(Soldier):
 
     def receiveDamage(self, damage):
         self.health -= damage
-        print('HOLA K ASE vikingo dañado ', self.health)
         if self.health > 0:
             return self.name + " has received " + str(damage) + " points of damage"
         else:
@@ -47,7 +46,6 @@ class Saxon(Soldier):
         return super().attack()
 
     def receiveDamage(self, damage):
-        print('HOLA K ASE saxon dañado ', self.health)
         self.health -= damage
         if self.health > 0:
             return "A Saxon has received " + str(damage) + " points of damage"
@@ -63,40 +61,32 @@ class War:
         self.saxonArmy = []
 
     def addViking(self, Viking):
-        print('Se va a crear un Viking: ', len(self.vikingArmy))
         self.vikingArmy.append(Viking)
-        print('Creado un Viking: ', len(self.vikingArmy))
 
     def addSaxon(self, Saxon):
-        print('Se va a crear un Saxon: ', len(self.saxonArmy))
         self.saxonArmy.append(Saxon)
-        print('Creado un Saxon: ', len(self.saxonArmy))
 
     def vikingAttack(self):
         import random
         randomSaxon = random.choice(self.saxonArmy)
-        randomViking = random.choice(self.saxonArmy)
-        randomSaxon.receiveDamage(randomViking.strength)
+        randomViking = random.choice(self.vikingArmy)
+        status_saxon = randomSaxon.receiveDamage(randomViking.strength)
 
         if randomSaxon.health <= 0:
-            print('Se va a eliminar saxon', randomSaxon.health)
-            print('Longitud saxonArmy antes: ', len(self.saxonArmy))
             self.saxonArmy.remove(randomSaxon)
-            print('ELIMINADO saxon')
-            print('Longitud saxonArmy después: ', len(self.saxonArmy))
+
+        return status_saxon
 
     def saxonAttack(self):
         import random
         randomViking = random.choice(self.vikingArmy)
         randomSaxon = random.choice(self.saxonArmy)
-        randomViking.receiveDamage(randomSaxon.strength)
+        status_viking = randomViking.receiveDamage(randomSaxon.strength)
 
         if randomViking.health <= 0:
-            print('Se va a eliminar al vikingo', randomViking.health)
-            print('Longitud vikingArmy antes: ', len(self.vikingArmy))
             self.vikingArmy.remove(randomViking)
-            print('Vikingo eliminado')
-            print('Longitud vikingArmy después: ', len(self.vikingArmy))
+
+        return status_viking
 
     def showStatus(self):
         if len(self.saxonArmy) == 0:
